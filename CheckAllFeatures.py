acbc1a140046cac8f0fd918299590d5dac0ece39
Boaz <bendboaz@gmail.com>
2020-03-13 11:44:00 +0200

    Added CIV computations.

--- CheckAllFeatures.py
+++ CheckAllFeatures.py
@@ -65,12 +65,14 @@ if __name__ == "__main__":
     result_path = os.path.join(PROJECT_ROOT, 'data', 'results', results_name)
     results = read_csv(result_path)
     results = results.rename({results.columns[0]: 'Description'}, axis='columns')
+    results['IV-based_effect'] = results['IV->Y'] / results['IV->T']
     all_aggregations = aggregate_estimators(results)
     all_aggregations['total'] = aggregate_experiments(results.drop(['est_name'], axis=1))
 
     final_df = pd.concat(all_aggregations).unstack()
     print(final_df['T->Y'].transpose().to_latex())
     print(final_df['IV->T'].transpose().to_latex())
+    print(final_df['IV-based_effect'].transpose().to_latex())
 
     data_path = os.path.join(PROJECT_ROOT, 'data')
     big_df = get_big_dataframe(data_path)
@@ -79,6 +81,10 @@ if __name__ == "__main__":
     treatment_col = 'obesity_percentage'
     eng_df = get_engineered_dataframe(data_path, 'all_states', big_df, 'totalScore',
                                       cols_to_keep=[label_col, treatment_col], cols_to_convert=[(treatment_col, 0.5)])
+    print(f"Correlation between IV and T:", eng_df['obesity_percentage'].corr(eng_df['pop_/n_re']))
+    print(f"Correlation between IV and Y:", eng_df['totalScore'].corr(eng_df['pop_/n_re']))
+
+    print("ATE computed on aggregated effects:", final_df['IV->Y']['mean']/final_df['IV->T']['mean'])
     iv_col = 'pop_/n_re'
     print(compute_iv_sem_estimate(eng_df, label_col, treatment_col, iv_col))
     print(compute_iv_constant_estimate(eng_df, label_col, treatment_col, iv_col))
